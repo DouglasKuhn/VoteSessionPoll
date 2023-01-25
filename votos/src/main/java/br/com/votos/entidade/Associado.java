9c88af8e0b0bf52d59f480ecfd06c1482fc53aa9
DouglasKuhn <43782830+DouglasKuhn@users.noreply.github.com>
2023-01-25 10:44:56 -0300

    Add files via upload

---
+++ votos/src/main/java/br/com/votos/entidade/Associado.java
@@ -0,0 +1,4 @@
+package br.com.votos.entidade;
+
+public class Associado {
+}
---
+++ votos/src/test/java/br/com/votos/VotosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
